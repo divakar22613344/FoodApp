70c070011597f58795ee644ad3d1ac9e23356e72
Divakar Chakravorty <divakarc72@gmail.com>
2026-01-20 08:11:51 +0530

    Food-React-App-Front-End

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>Your app is running successfully 🚀</p>
+    </div>
+  );
+}
+
+export default App;
